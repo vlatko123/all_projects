e53912955628cab003e1b5139cd148009d07cd32
vlatko123 <vlatkobogojevski@yahoo.com>
2020-05-13 14:38:51 +0200

    added project12

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "280px"; 
+  
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+  
+
+
+  
